4dd689b892d7c99ec8481f1249d25f6d5a853df4
AudreyBouxin <33879845+AudreyBouxin@users.noreply.github.com>
2018-05-22 18:06:59 +0200

    Design up to DM
    Zemax design :
    alpha OAP0/1 = 20°, fOAP0/1 = 445mm
    
    Report : jusqu'au DM tilté pour compensé tilt OAP0

--- DimensionnementOAPs.py
+++ DimensionnementOAPs.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri May 18 10:27:43 2018
-
+This code compute the focal length of the OAPs and the image position.
 @author: audrey.bouxin
 """
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 " Parameters"
 DEG2RAD = np.pi/180;
-#theta  = 15*DEG2RAD;	#[rad]  pour l'OAP0
 theta_OAP0  = 25*DEG2RAD;	#[rad]  pour l'OAP0
 theta_OAP1 = theta_OAP0;
 #theta_OAP2  = 10*DEG2RAD;	#[rad]  pour l'OAP2
@@ -19,17 +17,17 @@ ExP2FP = 10338.74;      #[mm]
 dExP   = 727.4046;      #[mm]
 DDM    = 32.7;          #[mm]
 
-
-
-# pour l'OAP0
+# for OAP0
 alpha = np.arctan(dExP*0.5/ExP2FP);  #[rad] 
-# PFL_OAP0_demi =(DDM/2)/2/(-1/tan(theta+alpha)+(1/(tan(theta+alpha))**2+1)**0.5+1/tan(theta)-(1/(tan(theta))**2+1)**0.5)   #[mm] pour l'OAP0
 PFL_OAP0 =(DDM)/2/(-1/np.tan(theta_OAP1+alpha)+(1/(np.tan(theta_OAP1+alpha))**2+1)**0.5+1/np.tan(theta_OAP1-alpha)-(1/(np.tan(theta_OAP1-alpha))**2+1)**0.5);   #[mm] pour l'OAP0
 EFL_OAP0 = 2*PFL_OAP0/(1+np.cos(theta_OAP1));
-print('PFL_OAP0 : ', PFL_OAP0)
-print('EFL_OAP0 : ', EFL_OAP0)
+print('PFL_OAP0 (estim) : ', PFL_OAP0)
+
  
-PFL_OAP0 = 440;
+PFL_OAP0 = 445.0;
+print('PFL_OAP0 : ', PFL_OAP0)
+EFL_OAP0 = 2*PFL_OAP0/(1+np.cos(theta_OAP1))
+print('EFL_OAP0 : ', EFL_OAP0)
 po = -(PFL_OAP0+ExP2FP);
 pi = po*PFL_OAP0/(po+PFL_OAP0);
 print('pi       : ', pi)
