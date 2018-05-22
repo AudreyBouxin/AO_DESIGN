6b0a980aa1e3d3a919e0755bc5d48d7346bf825e
AudreyBouxin <33879845+AudreyBouxin@users.noreply.github.com>
2018-05-22 13:08:24 +0200

    First version

---
+++ DimensionnementOAPs.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 18 10:27:43 2018
+
+@author: audrey.bouxin
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+" Parameters"
+DEG2RAD = np.pi/180;
+#theta  = 15*DEG2RAD;	#[rad]  pour l'OAP0
+theta_OAP0  = 25*DEG2RAD;	#[rad]  pour l'OAP0
+theta_OAP1 = theta_OAP0;
+#theta_OAP2  = 10*DEG2RAD;	#[rad]  pour l'OAP2
+#theta_OAP3  = 10*DEG2RAD;	#[rad]  pour l'OAP3
+ExP2FP = 10338.74;      #[mm]
+dExP   = 727.4046;      #[mm]
+DDM    = 32.7;          #[mm]
+
+
+
+# pour l'OAP0
+alpha = np.arctan(dExP*0.5/ExP2FP);  #[rad] 
+# PFL_OAP0_demi =(DDM/2)/2/(-1/tan(theta+alpha)+(1/(tan(theta+alpha))**2+1)**0.5+1/tan(theta)-(1/(tan(theta))**2+1)**0.5)   #[mm] pour l'OAP0
+PFL_OAP0 =(DDM)/2/(-1/np.tan(theta_OAP1+alpha)+(1/(np.tan(theta_OAP1+alpha))**2+1)**0.5+1/np.tan(theta_OAP1-alpha)-(1/(np.tan(theta_OAP1-alpha))**2+1)**0.5);   #[mm] pour l'OAP0
+EFL_OAP0 = 2*PFL_OAP0/(1+np.cos(theta_OAP1));
+print('PFL_OAP0 : ', PFL_OAP0)
+print('EFL_OAP0 : ', EFL_OAP0)
+ 
+PFL_OAP0 = 440;
+po = -(PFL_OAP0+ExP2FP);
+pi = po*PFL_OAP0/(po+PFL_OAP0);
+print('pi       : ', pi)
+# 
+# # pour l'OAP1
+# fOAP1 = 430;            #[mm]
+# alpha = atan(DDM*0.5/fOAP1);    #[rad] 
+# PFL_OAP1 =(DDM/2)/2/(-1/tan(theta_OAP1+alpha)+(1/(tan(theta_OAP1+alpha))**2+1)**0.5+1/tan(theta_OAP1)-(1/(tan(theta_OAP1))**2+1)**0.5);   #[mm] pour l'OAP1
+# EFL_OAP1 = 2*PFL_OAP1/(1+cos(theta_OAP1));
+# 
+# 
+# # pour l'OAP2
+# TTangle = 10*DEG2RAD;
+# DTT = 25.4*cos(TTangle);            #[mm]
+# fOAP2 = DTT/DDM*fOAP1;     #[-]
+# alpha = atan(DTT*0.5/fOAP2);    #[rad] 
+# PFL_OAP2 = (DTT/2)/2/(-1/tan(theta_OAP2+alpha)+(1/(tan(theta_OAP2+alpha))**2+1)**0.5+1/tan(theta_OAP2)-(1/(tan(theta_OAP2))**2+1)**0.5)  #[mm] pour l'OAP2
+# EFL_OAP2 = 2*PFL_OAP2/(1+cos(theta_OAP2))
+# 
+# # Pour l'OAP3
+# pyr_roof = 20;  #[um]
+# lambda   = 0.6; #[um]
+# fnum = 60;#2*pyr_roof/lambda;
+# alpha = atan(1/(fnum*2));       #[rad] pour l'OAP3
+# DTT = 25.11;
+# PFL_OAP3 = (DTT/2)/2/(-1/tan(theta_OAP3+alpha)+(1/(tan(theta_OAP3+alpha))**2+1)**0.5+1/tan(theta_OAP3)-(1/(tan(theta_OAP3))**2+1)**0.5)  #[mm] pour l'OAP3
+# EFL_OAP3 = 2*PFL_OAP3/(1+cos(theta_OAP3))
+# 
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # AO_DESIGN
+Here are the codes and simulations for the design of the AO.
+
+The Dimension code shows the calculations made to compute the OAP focal length and distances.
+
+The Zemax file present the current status of the AO design.
+The wavelength range is ???
+The field of view is 1 arcmin (FoV/2 = 1/2x1/60=0.008333333°) before the dichroic and 8'' ((FoV/2 = 1/2x1/3600=0.00111111°) in the WFS path.
